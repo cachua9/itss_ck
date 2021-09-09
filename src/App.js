2cd786056eb3d9f5f23efc85a73c7cae9c5af668
Hoang Lam <meolam1999@gmail.com>
2021-09-09 08:33:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import useStorage from "./hooks/useStorage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [index, inputName, handleChange, onClick] = useStorage();
+
+  return(
+    <div>
+    <div className="listName listStudent">
+      <p>学生一覧 : [Huyen, Hoa, Hung, Long]</p>
     </div>
+    <div className="listStudent" >
+    検索名前 :&nbsp;
+    <input onChange={handleChange} />
+    </div>
+    
+    <button className="myButton" onClick={onClick}>確定</button>
+    <div className="listStudent">検索名前: {inputName}</div>
+
+    <div className="listStudent">
+      位置: {
+        index === -1 ? (
+          <a></a>
+        ) : (
+          index === 0 ? (
+            <a>NaN</a>
+          ) : (
+            <a>{index}</a>
+          )
+        )
+      }
+    </div>
+  </div>
   );
 }
 
---
+++ src/hooks/useStorage.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+const useStorage = () => {
+    const [listStudent, setListStudent] = useState(["Huyen", "Hoa", "Hung", "Long"]);
+    const [inputName, setInputName] = useState("");
+    const [index, setIndex] = useState(-1);
+
+    const handleChange = (e) => {
+        setInputName(e.target.value);
+    };
+
+    const onClick = (e) => {
+        e.preventDefault();
+        setIndex(0);
+        listStudent.map((name, idx) => {
+            if(name == inputName) {
+                setIndex(idx + 1);
+            }
+        })
+        // setInputName("")
+    };
+
+    return [index, inputName, handleChange, onClick];
+};
+
+export default useStorage;
